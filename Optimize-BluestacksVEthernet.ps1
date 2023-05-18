39b6bf41bb94c987d7a6763aeccbe509d19f3318
Chris K.Y. FUNG <8746768+chriskyfung@users.noreply.github.com>
2023-05-18 15:28:43 +0800

    Add comments

---
+++ Optimize-BluestacksVEthernet.ps1
@@ -0,0 +1,62 @@
+#Requires -RunAsAdministrator
+
+<#
+.SYNOPSIS
+  Optimize Virtual Ethernet Adapter Performance for BlueStacks with Hyper-V Enabled
+
+.DESCRIPTION
+  This script will perform the following operations:
+  1. Disable Large Send Offload (LSO), Receive side scaling (RSS) and eceive segment coalescing (RSC) for all Virtual Ethernet Adapters.
+  2. Disable TCP/IPv6, File and Printer Sharing for Microsoft Networks, Link-Layer Topology Discovery Mapper I/O Driver and Link-Layer Topology Discovery Responder for the network adapter named "vEthernet (BluestacksNxt)".
+  3. Set the Receive Side Scaling (RSS) parameters for the network adapter named "vEthernet (BluestacksNxt)", -NumberOfReceiveQueues 1 -Profile "NUMAStatic" -MaxProcessors 4
+
+.OUTPUTS
+  None
+  
+.NOTES
+  Version:        1.1.2
+  Author:         chriskyfung
+  Website:        https://chriskyfung.github.io
+  Creation Date:  2023-03-09
+  Last Modified:  2023-05-18
+#>
+
+Try {
+
+  # Disable all Virtual Ethernet Adapters except the Virtual Ethernet Adapter for BlueStacks
+  Get-NetAdapter -Name "vEthernet *" | where Name -inotmatch "BluestacksNxt" | Disable-NetAdapter -Confirm:$false
+  # Ensure the Virtual Ethernet Adapter for BlueStacks is active
+  Enable-NetAdapter -Name "vEthernet (BluestacksNxt)"
+
+  # Disable Large Send Offload (LSO) for all Virtual Ethernet Adapters. Learn more: https://learn.microsoft.com/en-us/windows-hardware/drivers/network/performance-in-network-adapters#supporting-large-send-offload-lso
+  Disable-NetAdapterLso -Name "vEthernet (*)"
+  # Disable Receive side scaling (RSS) for all Virtual Ethernet Adapters. Learn more: https://learn.microsoft.com/en-us/windows-hardware/drivers/network/introduction-to-receive-side-scaling?source=recommendations
+  Disable-NetAdapterRss -Name "vEthernet (*)"
+  # Disable Receive segment coalescing (RSC) for all Virtual Ethernet Adapters. Learn more: https://learn.microsoft.com/en-us/windows-hardware/drivers/network/overview-of-receive-segment-coalescing
+  Disable-NetAdapterRsc -Name "vEthernet (*)"
+  
+  # Disable specific adapter bindings on vEthernet (BluestacksNxt). Learn more: https://learn.microsoft.com/en-us/powershell/module/netadapter/disable-netadapterbinding
+  Disable-NetAdapterBinding -Name "vEthernet (BluestacksNxt)" -ComponentID @("ms_tcpip6", "ms_server", "ms_lltdio", "ms_rspndr")
+  # Set Sets the RSS properties on vEthernet (BluestacksNxt). Learn more: https://learn.microsoft.com/en-us/powershell/module/netadapter/set-netadapterrss
+  Set-NetAdapterRss -Name "vEthernet (BluestacksNxt)" -NumberOfReceiveQueues 1 -Profile "NUMAStatic" -MaxProcessorNumber 3 -MaxProcessors 4
+
+  # List all acitve network adapters
+  (Get-NetAdapter | where status -eq "up" | Format-List -Property Name, InterfaceDescription, Status | Out-String).TrimEnd()
+
+  Write-Host "`nvEthernet (BluestacksNxt) - Large Send Offload (LSO):`n"
+  (Get-NetAdapterLso -Name "vEthernet (BluestacksNxt)" | Format-List -Property "*Enabled" | Out-String).Trim()
+
+  Write-Host "`nvEthernet (BluestacksNxt) - Receive Side Scaling (RSS):`n"
+  (Get-NetAdapterRss -Name "vEthernet (BluestacksNxt)" | Format-List -Property "Enabled" | Out-String).Trim()
+
+  Write-Host "`nvEthernet (BluestacksNxt) - Receive Segment Coalescing (RSC):`n"
+  (Get-NetAdapterRsc -Name "vEthernet (BluestacksNxt)" | Format-List -Property "*Enabled" | Out-String).Trim()
+  
+  Write-Host "`n"
+
+} Catch {
+
+  Write-Host "An error occurred:"
+  Write-Host $_
+
+}
---
+++ Resize-TheBrainNotesYouTubeThumbnail.ps1
@@ -0,0 +1,62 @@
+﻿<#
+.SYNOPSIS
+  Resize YouTube thumbnails down to 30% in theBrain Notes
+
+.DESCRIPTION
+  Append `#$width=30p$` to the image address within the Markdown files
+
+.OUTPUTS
+  None
+
+.NOTES
+  Version:        1.1.2
+  Author:         chriskyfung
+  Website:        https://chriskyfung.github.io
+  Creation Date:  2023-04-28
+  Last Modified:  2023-05-18
+#>
+
+# Enable Verbose output
+[CmdletBinding()]
+
+# Look up the Notes.md files that locate under the folder of My Documents\Brains and contain the YouTube thumbnail URLs.
+$BrainFolder = Join-Path -Path ([Environment]::GetFolderPath('MyDocuments')) -ChildPath 'Brains'
+$MatchInfo = Get-ChildItem -Path $BrainFolder -Filter 'Notes.md' -Recurse | Select-String '\/(hq|maxres)default.jpg\)' -List
+
+# For each matching result
+ForEach ($Match in $MatchInfo) {
+  $FilePath = $Match.Path | Convert-Path # FilePath of the Notes.md file
+  $ParentFolder = Split-Path -Path $FilePath # Path of the parent folder
+  # Check if any backup files exist in the parent folder
+  $NewIndex = 1
+  if (Test-Path -Path (-join ($FilePath, '.bak*')) -PathType leaf) {
+    # If true then determine the index of the last backup file
+    $LastIndex = (Get-ChildItem -Path $ParentFolder -Filter '*.bak*' | Select-Object Extension -Unique |
+                    Sort-Object -Property Extension | Select-Object -Last 1)[0] -replace ('\D*', '')
+    $NewIndex = [int]$LastIndex
+    # If there are more than three backup files
+    if ($NewIndex -ge 3) {
+      # Remove the first backup file and re-index the rest of the backup files
+      Remove-Item (-join ($FilePath, '.bak1'))
+      Rename-Item (-join ($FilePath, '.bak2')) -NewName (-join ($FilePath, '.bak1'))
+      Rename-Item (-join ($FilePath, '.bak3')) -NewName (-join ($FilePath, '.bak2'))
+    } else {
+      # Else increment the index for the new backup file
+      $NewIndex++
+    }
+  }
+  $NewName = -join ($FilePath, '.bak', $NewIndex) # FilePath of the new backup file
+  Copy-Item $FilePath -Destination $NewName # Backup the Notes.md file
+  Write-Verbose "Created -->' $NewName"
+  # Amend the link of the YouTube thumbnail with UTF8 encoding
+  $Pattern = $Match.Matches.Value
+  $NewString = $Pattern.Replace(')', '#$width=30p$)')
+  (Get-Content $FilePath -Encoding UTF8).Replace($Pattern, $NewString) | Set-Content $FilePath -Encoding UTF8
+  Write-Verbose "Modified -->' $FilePath"
+}
+
+Write-Host $MatchInfo.Length 'file(s) found' # Output the number of files found
+
+$MatchInfo | Format-Table Path | Out-Host # Output the path of the files found
+
+Return
